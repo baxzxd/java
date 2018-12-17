bd83070c19c0e02a8d9690b0877bdec5f1754e09
Adam Mason <masonadam6@gmail.com>
2018-12-17 18:00:32 -0500

    Added basic betting

--- crapsProject.java
+++ crapsProject.java
@@ -15,59 +15,76 @@ public class crapsProject
             Random rand = new Random();
         //Initialize Variables
             char progRun = 'p';
-            int diceSum = 0;
-            int point = 0;
-            int rollCounter = 0;
+            int diceSum, point, rollCounter, currentBet = 0;
+            int playerBalance = 1000;
+       
         //Greet player and ask for name
             System.out.print("Welcome to Adam's casino! Please enter your name to get started:");
             String playerName = scan.nextLine();
             System.out.println();
         while( progRun == 'p' )
         {   
-                //Ask user if they want to play again or quit and wait for input
-                    progRun = ' ';
-                while(progRun != 'p' && progRun != 'q')
+            currentBet = -1;
+            while(currentBet <= 0 || currentBet > playerBalance)
+            {
+                System.out.print("Enter a bet from your $" + playerBalance + " balance: ");
+                currentBet = scan.nextInt();
+            }
+            if( progRun == 'p')
+            {
+                //Set roll count to 1, get new diceSum and print
+                    playerBalance -= currentBet;
+                    rollCounter = 1;
+                    diceSum = rand.nextInt(12) + 1;
+                    System.out.print("Roll 1: " + playerName + " rolls a " + diceSum + ". ");
+                if ( diceSum == 2 || diceSum == 3 || diceSum == 12 )
+                    System.out.println("You lost!");
+                else if ( diceSum == 7 || diceSum == 11 )
                 {
-                    System.out.print("Would you like to (p)lay or (q)uit?: ");
-                    progRun = scan.nextLine().toLowerCase().charAt(0);
-                    System.out.println();
+                    System.out.println("You won $" + (currentBet * 2) + "!");
+                    playerBalance += currentBet * 2;
                 }
-                if( progRun == 'p')
+                else 
                 {
-                    //Set roll count to 1, get new diceSum and print
-                        rollCounter = 1;
-                        diceSum = rand.nextInt(12) + 1;
-                        System.out.print("Roll 1: " + playerName + " rolls a " + diceSum + ". ");
-                    if ( diceSum == 2 || diceSum == 3 || diceSum == 12 )
-                        System.out.println("You lost!");
-                    else if ( diceSum == 7 || diceSum == 11 )
-                        System.out.println("You won!");
-                    else 
+                    //If diceSum does not make player win or lose set point equal to diceSum
+                        point = diceSum;
+                        System.out.print( "The point becomes " + point );
+                    //Roll until you get to break statement and break out
+                    while(true)
                     {
-                        //If diceSum does not make player win or lose set point equal to diceSum
-                            point = diceSum;
-                            System.out.print( "The point becomes " + point );
-                        //Roll until you get to break statement and break out
-                        while(true)
-                        {
-                            //Set roll count and diceSum
-                                rollCounter++;
-                                diceSum = rand.nextInt(12) + 1;
-                                System.out.print("\nRoll " + rollCounter + ": " + playerName + " rolls a " + diceSum + ". ");
-                            //Check if player lost or got the point, and then break
-                            if (diceSum == 7)
-                            {   
-                                System.out.println("You lost!");
-                                break;
-                            }
-                            else if (diceSum == point)
-                            {   
-                                System.out.println("You won!");
-                                break;
-                            }
+                        //Set roll count and diceSum
+                            rollCounter++;
+                            diceSum = rand.nextInt(12) + 1;
+                            System.out.print("\nRoll " + rollCounter + ": " + playerName + " rolls a " + diceSum + ". ");
+                        //Check if player lost or got the point, and then break
+                        if (diceSum == 7)
+                        {   
+                            System.out.println("You lost!");
+                            break;
+                        }
+                        else if (diceSum == point)
+                        {   
+                            System.out.println("You won $" + (currentBet * 2) + "!");
+                            playerBalance += currentBet * 2;
+                            break;
                         }
                     }
                 }
+            }
+            if(playerBalance <= 0)
+            {
+                System.out.println("You're broke!");
+                break;
+            }
+            //Ask user if they want to play again or quit and wait for input
+            progRun = ' ';
+            while(progRun != 'p' && progRun != 'q')
+            {
+                System.out.print("Would you like to (p)lay or (q)uit?: ");
+                scan.nextLine();
+                progRun = scan.nextLine().toLowerCase().charAt(0);
+                System.out.println();
+            }
         }
         System.out.println("Goodbye, " + playerName + "!");
     }   
